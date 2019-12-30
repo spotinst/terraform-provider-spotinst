0760972ffe9c00e8adf55847df2f59868d348021
saliHaguli <sali.haguli@spotinst.com>
2019-12-30 16:01:27 +0200

    [src] Support root vol size in our Ocean AWS launch spec TF resource

--- vendor/github.com/spotinst/spotinst-sdk-go/spotinst/version.go
+++ vendor/github.com/spotinst/spotinst-sdk-go/spotinst/version.go
@@ -1,7 +1,7 @@
 package spotinst
 
 // SDKVersion is the current version of the SDK.
-const SDKVersion = "1.38.1"
+const SDKVersion = "1.38.2"
 
 // SDKName is the name of the SDK.
 const SDKName = "spotinst-sdk-go"
