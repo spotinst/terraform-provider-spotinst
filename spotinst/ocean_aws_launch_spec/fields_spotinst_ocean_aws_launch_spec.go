6639d17dffc1b9c8a377f98a245c58e9a1d3f36f
liron.arad <liron.arad@spot.io>
2020-12-15 18:48:49 +0200

    roll back ocean aws image id enable null value

--- spotinst/ocean_aws_launch_spec/fields_spotinst_ocean_aws_launch_spec.go
+++ spotinst/ocean_aws_launch_spec/fields_spotinst_ocean_aws_launch_spec.go
@@ -84,11 +84,9 @@ func Setup(fieldsMap map[commons.FieldName]*commons.GenericField) {
 		func(resourceObject interface{}, resourceData *schema.ResourceData, meta interface{}) error {
 			launchSpecWrapper := resourceObject.(*commons.LaunchSpecWrapper)
 			launchSpec := launchSpecWrapper.GetLaunchSpec()
-			var imageId *string = nil
 			if value, ok := resourceData.GetOk(string(ImageID)); ok && value != nil {
-				imageId = spotinst.String(resourceData.Get(string(ImageID)).(string))
+				launchSpec.SetImageId(spotinst.String(resourceData.Get(string(ImageID)).(string)))
 			}
-			launchSpec.SetImageId(imageId)
 			return nil
 		},
 		nil,
