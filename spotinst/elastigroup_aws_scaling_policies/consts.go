9b6dd6a6d83b9f5e2f3c62e3ae5fb513c3bb0be1
galt <galt@netapp.com>
2021-08-16 09:53:27 +0300

    eg_aws-scaling_metrics

--- spotinst/elastigroup_aws_scaling_policies/consts.go
+++ spotinst/elastigroup_aws_scaling_policies/consts.go
@@ -32,6 +32,9 @@ const (
 	IsEnabled           commons.FieldName = "is_enabled"
 	PredictiveMode      commons.FieldName = "predictive_mode"
 	MaxCapacityPerScale commons.FieldName = "max_capacity_per_scale"
+	StepAdjustments     commons.FieldName = "step_adjustments"
+	Action              commons.FieldName = "action"
+	Type                commons.FieldName = "type"
 
 	DimensionName  DimensionField = "name"
 	DimensionValue DimensionField = "value"
