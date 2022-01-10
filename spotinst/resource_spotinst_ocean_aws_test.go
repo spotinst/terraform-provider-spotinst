d9c2477ba6c9f21a74b20909baf8bc23b33e79f4
galt <galt@netapp.com>
2022-01-10 13:33:26 +0200

    ocean_aws_scheduling test

--- spotinst/resource_spotinst_ocean_aws_test.go
+++ spotinst/resource_spotinst_ocean_aws_test.go
@@ -672,14 +672,14 @@ func TestAccSpotinstOceanAWS_Scheduling(t *testing.T) {
 					testCheckOceanAWSExists(&cluster, resourceName),
 					testCheckOceanAWSAttributes(&cluster, clusterName),
 					resource.TestCheckResourceAttr(resourceName, "scheduled_task.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.shutdown_hours.0.is_enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.shutdown_hours.0.time_windows.#", "2"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.shutdown_hours.0.time_windows.0", "Fri:15:30-Sat:13:30"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.shutdown_hours.0.time_windows.1", "Sun:15:30-Mon:13:30"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.tasks.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.tasks.0.cron_expression", "testcron"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.tasks.0.is_enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "scheduled_task.161750964.tasks.0.task_type", "clusterRoll"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.shutdown_hours.0.is_enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.shutdown_hours.0.time_windows.#", "2"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.shutdown_hours.0.time_windows.0", "Fri:15:30-Sat:13:30"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.shutdown_hours.0.time_windows.1", "Sun:00:30-Sun:01:00"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.tasks.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.tasks.0.cron_expression", "testcron"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.tasks.0.is_enabled", "true"),
+					resource.TestCheckResourceAttr(resourceName, "scheduled_task.4105912767.tasks.0.task_type", "clusterRoll"),
 				),
 			},
 			{
@@ -719,7 +719,7 @@ const testSchedulingConfig_Update = `
   scheduled_task   {
     shutdown_hours  {
       is_enabled = true
-      time_windows = ["Fri:15:30-Sat:13:30","Sun:15:30-Mon:13:30"]
+      time_windows = ["Fri:15:30-Sat:13:30","Sun:00:30-Sun:01:00"]
     }
     tasks  {
       is_enabled = true
