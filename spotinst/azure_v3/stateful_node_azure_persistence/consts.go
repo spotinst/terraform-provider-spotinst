00f1be124e6a65f3c517b188b91f251c866b7c6e
TamirYaariSpot <tamiry@netapp.com>
2022-04-18 16:44:14 +0300

    additional fields

---
+++ spotinst/azure_v3/stateful_node_azure_persistence/consts.go
@@ -0,0 +1,8 @@
+package stateful_node_azure_persistence
+
+import "github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
+
+const (
+	OnDemandSizes commons.FieldName = "od_sizes"
+	SpotSizes     commons.FieldName = "spot_sizes"
+)
---
+++ spotinst/azure_v3/stateful_node_azure_vm_sizes/consts.go
@@ -0,0 +1,12 @@
+package stateful_node_azure_vm_sizes
+
+import "github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
+
+const (
+	Prefix = "azure_vm_sizes_"
+)
+
+const (
+	OnDemandSizes commons.FieldName = "od_sizes"
+	SpotSizes     commons.FieldName = "spot_sizes"
+)
