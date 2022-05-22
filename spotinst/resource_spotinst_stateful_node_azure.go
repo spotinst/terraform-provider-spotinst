55a861976a35aa831a1e2aaebc85a0f5e0f5856b
TamirYaariSpot <tamiry@netapp.com>
2022-05-22 13:11:44 +0300

    added v2 to new stateful node azure resource - including minor changes in resource that now noticed are needed

--- spotinst/resource_spotinst_stateful_node_azure.go
+++ spotinst/resource_spotinst_stateful_node_azure.go
@@ -19,8 +19,8 @@ import (
 	"log"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/spotinst/spotinst-sdk-go/service/stateful/providers/azure"
 	"github.com/spotinst/spotinst-sdk-go/spotinst"
 	"github.com/spotinst/spotinst-sdk-go/spotinst/client"
--- spotinst/resource_spotinst_stateful_node_azure_test.go
+++ spotinst/resource_spotinst_stateful_node_azure_test.go
@@ -3,8 +3,8 @@ package spotinst
 import (
 	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/spotinst/spotinst-sdk-go/service/stateful/providers/azure"
 	"github.com/spotinst/spotinst-sdk-go/spotinst"
 	"github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
