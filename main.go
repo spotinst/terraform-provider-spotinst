277e01cfe5703071fa3971753a2a0b30c1f20093
lirona <Liron.Arad@netapp.com>
2022-06-07 10:51:21 +0300

    elastigroup aws - removed default values in scaling policy

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/spotinst/terraform-provider-spotinst/spotinst"
+	"log"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: spotinst.Provider})
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return spotinst.Provider()
+		},
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/spotinst/spotinst", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
