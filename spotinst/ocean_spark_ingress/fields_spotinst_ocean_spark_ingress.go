e74a2bda3ea17e5ebda0a65916f636bb5149caf9
Thorsteinn Thorri Sigurdsson <thorsteinnth@gmail.com>
2022-12-12 09:15:39 +0100

    add more ocean spark ingress configs

--- spotinst/ocean_spark_ingress/fields_spotinst_ocean_spark_ingress.go
+++ spotinst/ocean_spark_ingress/fields_spotinst_ocean_spark_ingress.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/spotinst/spotinst-sdk-go/service/ocean/spark"
+	"github.com/spotinst/spotinst-sdk-go/spotinst"
 
 	"github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
 )
@@ -25,6 +26,87 @@ func Setup(fieldsMap map[commons.FieldName]*commons.GenericField) {
 						Optional: true,
 						Computed: true,
 						Elem:     &schema.Schema{Type: schema.TypeString},
+					},
+					string(Controller): {
+						Type:     schema.TypeList,
+						Optional: true,
+						Computed: false,
+						MaxItems: 1,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								string(Managed): {
+									Type:     schema.TypeBool,
+									Optional: true,
+									Computed: false,
+								},
+							},
+						},
+					},
+					string(LoadBalancer): {
+						Type:     schema.TypeList,
+						Optional: true,
+						Computed: false,
+						MaxItems: 1,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								string(ServiceAnnotations): {
+									Type:     schema.TypeMap,
+									Optional: true,
+									Computed: true,
+									Elem:     &schema.Schema{Type: schema.TypeString},
+								},
+								string(Managed): {
+									Type:     schema.TypeBool,
+									Optional: true,
+									Computed: false,
+								},
+								string(TargetGroupARN): {
+									Type:     schema.TypeString,
+									Optional: true,
+									Computed: false,
+								},
+							},
+						},
+					},
+					string(CustomEndpoint): {
+						Type:     schema.TypeList,
+						Optional: true,
+						Computed: false,
+						MaxItems: 1,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								string(Enabled): {
+									Type:     schema.TypeBool,
+									Optional: true,
+									Computed: false,
+								},
+								string(Address): {
+									Type:     schema.TypeString,
+									Optional: true,
+									Computed: false,
+								},
+							},
+						},
+					},
+					string(PrivateLink): {
+						Type:     schema.TypeList,
+						Optional: true,
+						Computed: false,
+						MaxItems: 1,
+						Elem: &schema.Resource{
+							Schema: map[string]*schema.Schema{
+								string(Enabled): {
+									Type:     schema.TypeBool,
+									Optional: true,
+									Computed: false,
+								},
+								string(VPCEndpointService): {
+									Type:     schema.TypeString,
+									Optional: true,
+									Computed: false,
+								},
+							},
+						},
 					},
 				},
 			},
@@ -85,6 +167,10 @@ func flattenIngress(ingress *spark.IngressConfig) []interface{} {
 	}
 	result := make(map[string]interface{})
 	result[string(ServiceAnnotations)] = flattenAnnotations(ingress.ServiceAnnotations)
+	result[string(Controller)] = flattenController(ingress.Controller)
+	result[string(LoadBalancer)] = flattenLoadBalancer(ingress.LoadBalancer)
+	result[string(CustomEndpoint)] = flattenCustomEndpoint(ingress.CustomEndpoint)
+	result[string(PrivateLink)] = flattenPrivateLink(ingress.PrivateLink)
 	return []interface{}{result}
 }
 
@@ -110,7 +196,87 @@ func expandIngress(data interface{}, nullify bool) (*spark.IngressConfig, error)
 		}
 	}
 
+	if v, ok := m[string(Controller)]; ok {
+		controller, err := expandController(v, nullify)
+		if err != nil {
+			return nil, err
+		}
+		ingress.SetController(controller)
+	} else if nullify {
+		ingress.SetController(nil)
+	}
+
+	if v, ok := m[string(LoadBalancer)]; ok {
+		loadBalancer, err := expandLoadBalancer(v, nullify)
+		if err != nil {
+			return nil, err
+		}
+		ingress.SetLoadBalancer(loadBalancer)
+	} else if nullify {
+		ingress.SetLoadBalancer(nil)
+	}
+
+	if v, ok := m[string(CustomEndpoint)]; ok {
+		customEndpoint, err := expandCustomEndpoint(v, nullify)
+		if err != nil {
+			return nil, err
+		}
+		ingress.SetCustomEndpoint(customEndpoint)
+	} else if nullify {
+		ingress.SetCustomEndpoint(nil)
+	}
+
+	if v, ok := m[string(PrivateLink)]; ok {
+		privateLink, err := expandPrivateLink(v, nullify)
+		if err != nil {
+			return nil, err
+		}
+		ingress.SetPrivateLink(privateLink)
+	} else if nullify {
+		ingress.SetPrivateLink(nil)
+	}
+
 	return ingress, nil
+}
+
+func flattenController(controller *spark.IngressConfigController) []interface{} {
+	if controller == nil {
+		return nil
+	}
+	result := make(map[string]interface{})
+	result[string(Managed)] = spotinst.BoolValue(controller.Managed)
+	return []interface{}{result}
+}
+
+func flattenLoadBalancer(loadBalancer *spark.IngressConfigLoadBalancer) []interface{} {
+	if loadBalancer == nil {
+		return nil
+	}
+	result := make(map[string]interface{})
+	result[string(ServiceAnnotations)] = flattenAnnotations(loadBalancer.ServiceAnnotations)
+	result[string(Managed)] = spotinst.BoolValue(loadBalancer.Managed)
+	result[string(TargetGroupARN)] = spotinst.StringValue(loadBalancer.TargetGroupARN)
+	return []interface{}{result}
+}
+
+func flattenCustomEndpoint(customEndpoint *spark.IngressConfigCustomEndpoint) []interface{} {
+	if customEndpoint == nil {
+		return nil
+	}
+	result := make(map[string]interface{})
+	result[string(Enabled)] = spotinst.BoolValue(customEndpoint.Enabled)
+	result[string(Address)] = spotinst.StringValue(customEndpoint.Address)
+	return []interface{}{result}
+}
+
+func flattenPrivateLink(privateLink *spark.IngressConfigPrivateLink) []interface{} {
+	if privateLink == nil {
+		return nil
+	}
+	result := make(map[string]interface{})
+	result[string(Enabled)] = spotinst.BoolValue(privateLink.Enabled)
+	result[string(VPCEndpointService)] = spotinst.StringValue(privateLink.VPCEndpointService)
+	return []interface{}{result}
 }
 
 func flattenAnnotations(annotations map[string]string) map[string]interface{} {
@@ -119,6 +285,107 @@ func flattenAnnotations(annotations map[string]string) map[string]interface{} {
 		result[k] = v
 	}
 	return result
+}
+
+// TODO(thorsteinn) should I be nullifying stuff at this level?
+func expandController(data interface{}, nullify bool) (*spark.IngressConfigController, error) {
+	controller := &spark.IngressConfigController{}
+	list := data.([]interface{})
+	if list == nil || list[0] == nil {
+		return controller, nil
+	}
+	m := list[0].(map[string]interface{})
+
+	if v, ok := m[string(Managed)].(bool); ok {
+		controller.SetManaged(spotinst.Bool(v))
+	} else if nullify {
+		controller.SetManaged(nil)
+	}
+
+	return controller, nil
+}
+
+func expandLoadBalancer(data interface{}, nullify bool) (*spark.IngressConfigLoadBalancer, error) {
+	loadBalancer := &spark.IngressConfigLoadBalancer{}
+	list := data.([]interface{})
+	if list == nil || list[0] == nil {
+		return loadBalancer, nil
+	}
+	m := list[0].(map[string]interface{})
+
+	if v, ok := m[string(Managed)].(bool); ok {
+		loadBalancer.SetManaged(spotinst.Bool(v))
+	} else if nullify {
+		loadBalancer.SetManaged(nil)
+	}
+
+	if v, ok := m[string(TargetGroupARN)].(string); ok {
+		loadBalancer.SetTargetGroupARN(spotinst.String(v))
+	} else if nullify {
+		loadBalancer.SetTargetGroupARN(nil)
+	}
+
+	if v, ok := m[string(ServiceAnnotations)]; ok {
+		annotations, err := expandAnnotations(v)
+		if err != nil {
+			return nil, err
+		}
+		if len(annotations) > 0 {
+			loadBalancer.SetServiceAnnotations(annotations)
+		} else {
+			if nullify {
+				loadBalancer.SetServiceAnnotations(nil)
+			}
+		}
+	}
+
+	return loadBalancer, nil
+}
+
+func expandCustomEndpoint(data interface{}, nullify bool) (*spark.IngressConfigCustomEndpoint, error) {
+	customEndpoint := &spark.IngressConfigCustomEndpoint{}
+	list := data.([]interface{})
+	if list == nil || list[0] == nil {
+		return customEndpoint, nil
+	}
+	m := list[0].(map[string]interface{})
+
+	if v, ok := m[string(Enabled)].(bool); ok {
+		customEndpoint.SetEnabled(spotinst.Bool(v))
+	} else if nullify {
+		customEndpoint.SetEnabled(nil)
+	}
+
+	if v, ok := m[string(Address)].(string); ok {
+		customEndpoint.SetAddress(spotinst.String(v))
+	} else if nullify {
+		customEndpoint.SetAddress(nil)
+	}
+
+	return customEndpoint, nil
+}
+
+func expandPrivateLink(data interface{}, nullify bool) (*spark.IngressConfigPrivateLink, error) {
+	privateLink := &spark.IngressConfigPrivateLink{}
+	list := data.([]interface{})
+	if list == nil || list[0] == nil {
+		return privateLink, nil
+	}
+	m := list[0].(map[string]interface{})
+
+	if v, ok := m[string(Enabled)].(bool); ok {
+		privateLink.SetEnabled(spotinst.Bool(v))
+	} else if nullify {
+		privateLink.SetEnabled(nil)
+	}
+
+	if v, ok := m[string(VPCEndpointService)].(string); ok {
+		privateLink.SetVPCEndpointService(spotinst.String(v))
+	} else if nullify {
+		privateLink.SetVPCEndpointService(nil)
+	}
+
+	return privateLink, nil
 }
 
 func expandAnnotations(data interface{}) (map[string]string, error) {
