3e7b0cbf26a816c26cc7928b42646d915c80103c
Thorsteinn Thorri Sigurdsson <thorsteinnth@gmail.com>
2022-12-13 14:40:53 +0100

    nullification + tests

--- spotinst/ocean_spark_ingress/fields_spotinst_ocean_spark_ingress.go
+++ spotinst/ocean_spark_ingress/fields_spotinst_ocean_spark_ingress.go
@@ -52,7 +52,7 @@ func Setup(fieldsMap map[commons.FieldName]*commons.GenericField) {
 								string(ServiceAnnotations): {
 									Type:     schema.TypeMap,
 									Optional: true,
-									Computed: true,
+									Computed: false,
 									Elem:     &schema.Schema{Type: schema.TypeString},
 								},
 								string(Managed): {
@@ -167,17 +167,30 @@ func flattenIngress(ingress *spark.IngressConfig) []interface{} {
 	}
 	result := make(map[string]interface{})
 	result[string(ServiceAnnotations)] = flattenAnnotations(ingress.ServiceAnnotations)
-	result[string(Controller)] = flattenController(ingress.Controller)
-	result[string(LoadBalancer)] = flattenLoadBalancer(ingress.LoadBalancer)
-	result[string(CustomEndpoint)] = flattenCustomEndpoint(ingress.CustomEndpoint)
-	result[string(PrivateLink)] = flattenPrivateLink(ingress.PrivateLink)
+
+	if ingress.Controller != nil {
+		result[string(Controller)] = flattenController(ingress.Controller)
+	}
+
+	if ingress.LoadBalancer != nil {
+		result[string(LoadBalancer)] = flattenLoadBalancer(ingress.LoadBalancer)
+	}
+
+	if ingress.CustomEndpoint != nil {
+		result[string(CustomEndpoint)] = flattenCustomEndpoint(ingress.CustomEndpoint)
+	}
+
+	if ingress.PrivateLink != nil {
+		result[string(PrivateLink)] = flattenPrivateLink(ingress.PrivateLink)
+	}
+
 	return []interface{}{result}
 }
 
 func expandIngress(data interface{}, nullify bool) (*spark.IngressConfig, error) {
 	ingress := &spark.IngressConfig{}
 	list := data.([]interface{})
-	if list == nil || list[0] == nil {
+	if len(list) == 0 || list[0] == nil {
 		return ingress, nil
 	}
 	m := list[0].(map[string]interface{})
@@ -197,13 +210,11 @@ func expandIngress(data interface{}, nullify bool) (*spark.IngressConfig, error)
 	}
 
 	if v, ok := m[string(Controller)]; ok {
-		controller, err := expandController(v, nullify)
+		controller, err := expandController(v)
 		if err != nil {
 			return nil, err
 		}
 		ingress.SetController(controller)
-	} else if nullify {
-		ingress.SetController(nil)
 	}
 
 	if v, ok := m[string(LoadBalancer)]; ok {
@@ -212,28 +223,22 @@ func expandIngress(data interface{}, nullify bool) (*spark.IngressConfig, error)
 			return nil, err
 		}
 		ingress.SetLoadBalancer(loadBalancer)
-	} else if nullify {
-		ingress.SetLoadBalancer(nil)
 	}
 
 	if v, ok := m[string(CustomEndpoint)]; ok {
-		customEndpoint, err := expandCustomEndpoint(v, nullify)
+		customEndpoint, err := expandCustomEndpoint(v)
 		if err != nil {
 			return nil, err
 		}
 		ingress.SetCustomEndpoint(customEndpoint)
-	} else if nullify {
-		ingress.SetCustomEndpoint(nil)
 	}
 
 	if v, ok := m[string(PrivateLink)]; ok {
-		privateLink, err := expandPrivateLink(v, nullify)
+		privateLink, err := expandPrivateLink(v)
 		if err != nil {
 			return nil, err
 		}
 		ingress.SetPrivateLink(privateLink)
-	} else if nullify {
-		ingress.SetPrivateLink(nil)
 	}
 
 	return ingress, nil
@@ -244,7 +249,9 @@ func flattenController(controller *spark.IngressConfigController) []interface{} 
 		return nil
 	}
 	result := make(map[string]interface{})
-	result[string(Managed)] = spotinst.BoolValue(controller.Managed)
+	if controller.Managed != nil {
+		result[string(Managed)] = spotinst.BoolValue(controller.Managed)
+	}
 	return []interface{}{result}
 }
 
@@ -253,9 +260,19 @@ func flattenLoadBalancer(loadBalancer *spark.IngressConfigLoadBalancer) []interf
 		return nil
 	}
 	result := make(map[string]interface{})
-	result[string(ServiceAnnotations)] = flattenAnnotations(loadBalancer.ServiceAnnotations)
-	result[string(Managed)] = spotinst.BoolValue(loadBalancer.Managed)
-	result[string(TargetGroupARN)] = spotinst.StringValue(loadBalancer.TargetGroupARN)
+
+	if loadBalancer.ServiceAnnotations != nil {
+		result[string(ServiceAnnotations)] = flattenAnnotations(loadBalancer.ServiceAnnotations)
+	}
+
+	if loadBalancer.Managed != nil {
+		result[string(Managed)] = spotinst.BoolValue(loadBalancer.Managed)
+	}
+
+	if loadBalancer.TargetGroupARN != nil {
+		result[string(TargetGroupARN)] = spotinst.StringValue(loadBalancer.TargetGroupARN)
+	}
+
 	return []interface{}{result}
 }
 
@@ -264,8 +281,15 @@ func flattenCustomEndpoint(customEndpoint *spark.IngressConfigCustomEndpoint) []
 		return nil
 	}
 	result := make(map[string]interface{})
-	result[string(Enabled)] = spotinst.BoolValue(customEndpoint.Enabled)
-	result[string(Address)] = spotinst.StringValue(customEndpoint.Address)
+
+	if customEndpoint.Enabled != nil {
+		result[string(Enabled)] = spotinst.BoolValue(customEndpoint.Enabled)
+	}
+
+	if customEndpoint.Address != nil {
+		result[string(Address)] = spotinst.StringValue(customEndpoint.Address)
+	}
+
 	return []interface{}{result}
 }
 
@@ -274,8 +298,15 @@ func flattenPrivateLink(privateLink *spark.IngressConfigPrivateLink) []interface
 		return nil
 	}
 	result := make(map[string]interface{})
-	result[string(Enabled)] = spotinst.BoolValue(privateLink.Enabled)
-	result[string(VPCEndpointService)] = spotinst.StringValue(privateLink.VPCEndpointService)
+
+	if privateLink.Enabled != nil {
+		result[string(Enabled)] = spotinst.BoolValue(privateLink.Enabled)
+	}
+
+	if privateLink.VPCEndpointService != nil {
+		result[string(VPCEndpointService)] = spotinst.StringValue(privateLink.VPCEndpointService)
+	}
+
 	return []interface{}{result}
 }
 
@@ -287,42 +318,35 @@ func flattenAnnotations(annotations map[string]string) map[string]interface{} {
 	return result
 }
 
-// TODO(thorsteinn) should I be nullifying stuff at this level?
-func expandController(data interface{}, nullify bool) (*spark.IngressConfigController, error) {
-	controller := &spark.IngressConfigController{}
+func expandController(data interface{}) (*spark.IngressConfigController, error) {
 	list := data.([]interface{})
-	if list == nil || list[0] == nil {
-		return controller, nil
+	if len(list) == 0 || list[0] == nil {
+		return nil, nil
 	}
+	controller := &spark.IngressConfigController{}
 	m := list[0].(map[string]interface{})
 
 	if v, ok := m[string(Managed)].(bool); ok {
 		controller.SetManaged(spotinst.Bool(v))
-	} else if nullify {
-		controller.SetManaged(nil)
 	}
 
 	return controller, nil
 }
 
 func expandLoadBalancer(data interface{}, nullify bool) (*spark.IngressConfigLoadBalancer, error) {
-	loadBalancer := &spark.IngressConfigLoadBalancer{}
 	list := data.([]interface{})
-	if list == nil || list[0] == nil {
-		return loadBalancer, nil
+	if len(list) == 0 || list[0] == nil {
+		return nil, nil
 	}
+	loadBalancer := &spark.IngressConfigLoadBalancer{}
 	m := list[0].(map[string]interface{})
 
 	if v, ok := m[string(Managed)].(bool); ok {
 		loadBalancer.SetManaged(spotinst.Bool(v))
-	} else if nullify {
-		loadBalancer.SetManaged(nil)
 	}
 
 	if v, ok := m[string(TargetGroupARN)].(string); ok {
 		loadBalancer.SetTargetGroupARN(spotinst.String(v))
-	} else if nullify {
-		loadBalancer.SetTargetGroupARN(nil)
 	}
 
 	if v, ok := m[string(ServiceAnnotations)]; ok {
@@ -342,47 +366,39 @@ func expandLoadBalancer(data interface{}, nullify bool) (*spark.IngressConfigLoa
 	return loadBalancer, nil
 }
 
-func expandCustomEndpoint(data interface{}, nullify bool) (*spark.IngressConfigCustomEndpoint, error) {
-	customEndpoint := &spark.IngressConfigCustomEndpoint{}
+func expandCustomEndpoint(data interface{}) (*spark.IngressConfigCustomEndpoint, error) {
 	list := data.([]interface{})
-	if list == nil || list[0] == nil {
-		return customEndpoint, nil
+	if len(list) == 0 || list[0] == nil {
+		return nil, nil
 	}
+	customEndpoint := &spark.IngressConfigCustomEndpoint{}
 	m := list[0].(map[string]interface{})
 
 	if v, ok := m[string(Enabled)].(bool); ok {
 		customEndpoint.SetEnabled(spotinst.Bool(v))
-	} else if nullify {
-		customEndpoint.SetEnabled(nil)
 	}
 
 	if v, ok := m[string(Address)].(string); ok {
 		customEndpoint.SetAddress(spotinst.String(v))
-	} else if nullify {
-		customEndpoint.SetAddress(nil)
 	}
 
 	return customEndpoint, nil
 }
 
-func expandPrivateLink(data interface{}, nullify bool) (*spark.IngressConfigPrivateLink, error) {
-	privateLink := &spark.IngressConfigPrivateLink{}
+func expandPrivateLink(data interface{}) (*spark.IngressConfigPrivateLink, error) {
 	list := data.([]interface{})
-	if list == nil || list[0] == nil {
-		return privateLink, nil
+	if len(list) == 0 || list[0] == nil {
+		return nil, nil
 	}
+	privateLink := &spark.IngressConfigPrivateLink{}
 	m := list[0].(map[string]interface{})
 
 	if v, ok := m[string(Enabled)].(bool); ok {
 		privateLink.SetEnabled(spotinst.Bool(v))
-	} else if nullify {
-		privateLink.SetEnabled(nil)
 	}
 
 	if v, ok := m[string(VPCEndpointService)].(string); ok {
 		privateLink.SetVPCEndpointService(spotinst.String(v))
-	} else if nullify {
-		privateLink.SetVPCEndpointService(nil)
 	}
 
 	return privateLink, nil
