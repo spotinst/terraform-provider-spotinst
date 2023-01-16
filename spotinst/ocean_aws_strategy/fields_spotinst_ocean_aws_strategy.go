69d5856e8a8babbd23635a78c84b9eb8d09e49ff
sharadkesarwani <sharad.kesarwani@netapp.com>
2023-01-16 19:56:06 +0530

    updated go sdk version
    updated go sdk version

--- spotinst/ocean_aws_strategy/fields_spotinst_ocean_aws_strategy.go
+++ spotinst/ocean_aws_strategy/fields_spotinst_ocean_aws_strategy.go
@@ -2,6 +2,7 @@ package ocean_aws_strategy
 
 import (
 	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/spotinst/spotinst-sdk-go/service/ocean/providers/aws"
