45bf628c3ba7543bf06f603e8d72b3bf50a212db
sharadkesarwani <sharad.kesarwani@netapp.com>
2023-04-13 15:17:00 +0530

    Updated testcase to validate tag having value as terraform-automation
    Updated testcase to validate tag having value as terraform-automation

--- spotinst/resource_spotinst_ocean_aws_test.go
+++ spotinst/resource_spotinst_ocean_aws_test.go
@@ -394,7 +394,7 @@ func TestAccSpotinstOceanAWS_LaunchConfiguration(t *testing.T) {
 					//resource.TestCheckResourceAttr(resourceName, "iam_instance_profile", "iam-profile"),
 					resource.TestCheckResourceAttr(resourceName, "tags.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "tags.0.key", "creator"),
-					resource.TestCheckResourceAttr(resourceName, "tags.0.value", "fakeValue"),
+					resource.TestCheckResourceAttr(resourceName, "tags.0.value", "terraform-automation"),
 					resource.TestCheckResourceAttr(resourceName, "load_balancers.#", "2"),
 					resource.TestCheckResourceAttr(resourceName, "load_balancers.0.arn", "arn:aws:elasticloadbalancing:us-west-2:123456789012:targetgroup/testTargetGroup/1234567890123456"),
 					resource.TestCheckResourceAttr(resourceName, "load_balancers.0.type", "TARGET_GROUP"),
