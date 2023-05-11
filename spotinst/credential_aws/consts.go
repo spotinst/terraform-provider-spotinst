2a7c37ec29ec97111c8110438901999da8b19768
sharadkesarwani <sharad.kesarwani@netapp.com>
2023-05-11 13:27:56 +0530

    updated variable name
    updated variable name

--- spotinst/credential_aws/consts.go
+++ spotinst/credential_aws/consts.go
@@ -3,6 +3,6 @@ package credential_aws
 import "github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
 
 const (
-	IamRole   commons.FieldName = "iamRole"
-	AccountId commons.FieldName = "accountId"
+	IamRole   commons.FieldName = "iam_role"
+	AccountId commons.FieldName = "account_id"
 )
