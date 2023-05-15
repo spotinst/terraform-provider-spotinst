86f4e2c4945ab9803a2d6f50483e8ba706331ab0
sharadkesarwani <sharad.kesarwani@netapp.com>
2023-05-15 14:43:14 +0530

    removed resource credential and its dependency
    removed resource credential and its dependency

--- spotinst/credential_aws/consts.go
+++
@@ -1,8 +0,0 @@
-package credential_aws
-
-import "github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
-
-const (
-	IamRole   commons.FieldName = "iam_role"
-	AccountId commons.FieldName = "account_id"
-)
