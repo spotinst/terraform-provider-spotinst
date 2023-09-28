231516b9543fb378fd762a2aa830997b957908ce
sharadkesarwani <sharad.kesarwani@netapp.com>
2023-09-28 21:40:06 +0530

    modified attribute name.
    modified attribute name.

--- spotinst/credentials_aws/consts.go
+++ spotinst/credentials_aws/consts.go
@@ -4,5 +4,5 @@ import "github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
 
 const (
 	IamRole   commons.FieldName = "iamrole"
-	AccountId commons.FieldName = "accountid"
+	AccountId commons.FieldName = "account_id"
 )
