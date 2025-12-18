e3c149f7d6e4b33225877b171dc0c8e896784c25
yasharma-spot-flexera <yasharma@flexera.com>
2025-12-18 19:42:28 +0530

    Revert "fix(Tests): Fixing TestAccSpotinstElastigroupAzureV3 test cases for Jenkins"
    This reverts commit ed795da528dfe4d8616b11851c49f01e55915567.

--- spotinst/resource_spotinst_elastigroup_azure_v3_test.go
+++ spotinst/resource_spotinst_elastigroup_azure_v3_test.go
@@ -365,9 +365,9 @@ func TestAccSpotinstElastigroupAzureV3_Image(t *testing.T) {
 					testCheckElastigroupAzureV3Attributes(&group, groupName),
 					resource.TestCheckResourceAttr(resourceName, "image.#", "1"),
 					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.#", "1"),
-					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.0.offer", "0001-com-ubuntu-server-jammy"),
+					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.0.offer", "ubuntu-24_04-lts"),
 					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.0.publisher", "Canonical"),
-					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.0.sku", "22_04-lts-gen2"),
+					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.0.sku", "server"),
 					resource.TestCheckResourceAttr(resourceName, "image.0.marketplace.0.version", "latest"),
 				),
 			},
@@ -380,8 +380,8 @@ const testAzureV3ImageGroupConfig_Create = `
   image {
     marketplace {
       publisher = "Canonical"
-      offer = "0001-com-ubuntu-server-jammy"
-      sku = "22_04-lts-gen2"
+      offer = "ubuntu-24_04-lts"
+      sku = "server"
       version = "latest"
     }
   }
