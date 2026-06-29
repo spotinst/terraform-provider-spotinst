62e957303402dde45d39dfa3b1718fdf9fe3d110
yasharma-spot-flexera <yasharma@flexera.com>
2026-06-29 20:10:56 +0530

    feat(ocean Spark): Decommission OFAS SDK code and remove OFAS components.

--- spotinst/ocean_spark_spark/consts.go
+++
@@ -1,8 +0,0 @@
-package ocean_spark_spark
-
-import "github.com/spotinst/terraform-provider-spotinst/spotinst/commons"
-
-const (
-	Spark                   commons.FieldName = "spark"
-	AdditionalAppNamespaces commons.FieldName = "additional_app_namespaces"
-)
